9b2a8061b3f013ba8555ff599ac014e83513e056
stam <junkboy0315@gmail.com>
2018-09-11 14:40:59 +0900

    change json structure

--- data/type.ts
+++ data/type.ts
@@ -1,67 +1,14 @@
-// Award                    => 受賞
-// AwardStudents            => 指導学生の受賞
-// Book                     => Books
-// ConferenceDomestic       => 研究会、全国大会
-// ConferenceInternational  => 国際会議
-// Other                    => 招待講演・その他
-// Paper                    => 論文誌
-
-type Award = {
-  awardName: string;
-  title?: string;
-  organization: string;
-  year: string;
-  slides?: string;
-  video?: string;
-  github?: string;
-};
-
-type AwardStudents = {
-  awardName: string;
-  title: string;
-  students: string[];
-  year: string;
-};
-
-type Book = {
+type Publications = {
   title: string;
   authors: string[];
-  TODO_what_is_this: string;
-};
-
-type ConferenceDomestic = {
-  title: string;
-  authors: string[];
-  conference: string;
-  conferenceUrl?: string;
-  year: string;
-  award?: string;
-};
-
-type ConferenceInternational = {
-  title: string;
-  authors: string;
-  conference: string;
-  conferenceUrl?: string;
-  year: string;
-  award?: string;
-  slides?: string;
-  video?: string;
-  pdf?: string;
-  bibtex?: string;
-};
-
-type Other = {
-  title: string;
-  at: string;
-  atUrl?: string;
-  yearOrDate: string;
-  slides?: string;
-};
-
-type Paper = {
-  title: string;
-  authors: string[];
-  press: string;
-  url?: string;
+  journal: string;
+  date: string; // yearのみの指定も可
+  notes: string[];
+  links: {
+    detail: string; // 論文詳細へのリンク、講演会詳細へのリンク、学会HPへのリンク
+    slides?: string;
+    video?: string;
+    pdf?: string;
+    bibtex?: string;
+  };
 };

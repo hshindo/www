90b4a2fd8579366e49ac5d289ed437ca5925bf3a
stam <junkboy0315@gmail.com>
2018-09-11 16:36:49 +0900

    change `date` format

--- data/type.ts
+++ data/type.ts
@@ -2,7 +2,7 @@ type Publications = {
   title: string;
   authors: string[];
   journal: string;
-  date: string; // yearのみの指定も可
+  date: string; // '2018' or '2018-09' or '2018-09-10'
   notes: string[];
   links: {
     detail: string; // 論文詳細へのリンク、講演会詳細へのリンク、学会HPへのリンク

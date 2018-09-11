01d09379f4689c28b1fb1a05135a8450f4b48047
stam <junkboy0315@gmail.com>
2018-09-11 16:37:18 +0900

    update type

--- data/type.ts
+++ data/type.ts
@@ -5,7 +5,7 @@ type Publications = {
   date: string; // '2018' or '2018-09' or '2018-09-10'
   notes: string[];
   links: {
-    detail: string; // 論文詳細へのリンク、講演会詳細へのリンク、学会HPへのリンク
+    detail?: string; // 論文詳細へのリンク、講演会詳細へのリンク、学会HPへのリンク
     slides?: string;
     video?: string;
     pdf?: string;

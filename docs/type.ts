03b3604a31ec58e92dd5a4f8f27b7480024eb44c
stam <junkboy0315@gmail.com>
2018-09-12 13:55:35 +0900

    split `detail` to `title` and `booktitle`

--- docs/type.ts
+++ docs/type.ts
@@ -5,7 +5,8 @@ type Publication = {
   date: string; // '2018' or '2018-09' or '2018-09-10'
   notes: string[];
   links: {
-    detail?: string; // 論文詳細へのリンク、講演会詳細へのリンク、学会HPへのリンク
+    title?: string;
+    booktitle?: string;
     slides?: string;
     video?: string;
     pdf?: string;
